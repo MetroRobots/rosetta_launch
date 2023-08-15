64d5f9958849f725d7efe2f44ce1d7cba6e143fe
David V. Lu!! <davidvlu@gmail.com>
2023-08-15 14:54:40 -0400

    Use FindPackageShare (#6)

--- donatello/launch/06c-substitutions.launch.py
+++ donatello/launch/06c-substitutions.launch.py
@@ -1,13 +1,13 @@
-from ament_index_python.packages import get_package_share_path
 import launch
 from launch.actions import DeclareLaunchArgument
 import launch_ros.actions
 from launch_ros.parameter_descriptions import ParameterFile
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
     file_parameters = ParameterFile(
-        param_file=str(get_package_share_path('donatello') / 'config/sub_params.yaml'),
+        param_file=[FindPackageShare('donatello'), '/config/sub_params.yaml'],
         allow_substs=True
     )
 
--- donatello/launch/09-dynamic-filename.launch.py
+++ donatello/launch/09-dynamic-filename.launch.py
@@ -1,12 +1,12 @@
-from ament_index_python.packages import get_package_share_directory
 import launch
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 import launch_ros.actions
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    dynamic_param_path = [get_package_share_directory('donatello'), '/config/', LaunchConfiguration('config'), '.yaml']
+    dynamic_param_path = [FindPackageShare('donatello'), '/config/', LaunchConfiguration('config'), '.yaml']
 
     return launch.LaunchDescription([
         DeclareLaunchArgument('config', default_value='params'),
