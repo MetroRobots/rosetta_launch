f7ea5409c5c3a939506e015a8435f0d94ce2020a
David V. Lu!! <davidvlu@gmail.com>
2026-06-16 15:31:46 -0400

    Set Global Parameter (#28)

--- donatello/launch/python/02-param.launch.py
+++ donatello/launch/python/02-param.launch.py
@@ -1,9 +1,10 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetParameter
 
 
 def generate_launch_description():
     return LaunchDescription([
+        SetParameter(name='use_sim_time', value='true'),
         Node(name='does_machines',
              package='donatello',
              executable='donatello_node',
